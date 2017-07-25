27a2c587ecdd8449a16986ce37776f507d232665
pradeepkodumuri1 <pradhu.92@gmail.com>
2017-07-25 10:18:38 -0500

    first commit

---
+++ src/firstclass/Explainn.java
@@ -0,0 +1,24 @@
+package firstclass;
+
+public class Explainn extends MethodOver {
+
+	public int area(int x,int y)
+	{
+	return x*y;
+	}
+	public int area(int x)
+	{
+	return x-x;
+	}
+	public float area(float x,int y)
+	{
+	return x*y*y;
+	}
+
+	}
+
+
+
+
+	
+	
---
+++ src/firstclass/MethodOver.java
@@ -0,0 +1,20 @@
+package firstclass;
+
+public class MethodOver {
+
+	public static void main(String[] args) {
+		int A,B,radius;
+        float d;	
+        Explainn a= new Explainn();
+        A=10;
+        B=11;
+        radius= 5;
+        d= 3.14f;
+        System.out.println("Area of rectangle" + a.area(A,B));
+        System.out.println("Area of square" + a.area(A));
+        System.out.println("Area of circle" + a.area(d,radius));
+        
+
+	}
+
+}
